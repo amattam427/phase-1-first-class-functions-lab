65da14cc8e34ff403da756fc71c141ec3b695d23
Ashley Mattam <amattam427@gmail.com>
2021-11-24 13:50:40 -0500

    commit tree 58094ae712ebf306ca6d44a7bcc5b39ccc01e1c4

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia']
+const returnFirstTwoDrivers = function(){
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(){
+    return drivers.slice(2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(number){
+    const fareMultiplier = function(price){
+        return (number*price);
+    }
+    return fareMultiplier;
+}
+
+function fareDoubler(number){
+    const double = createFareMultiplier(number);
+    return double(2);
+}
+
+function fareTripler(number){
+    const triple = createFareMultiplier(number);
+    return triple(3);
+}
+
+function selectDifferentDrivers(drivers, parameter2){
+    return parameter2(drivers);
+}
+
